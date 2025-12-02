4edcd561699f8159b73331d3e16e638c9dcacafe
mantu01714 <mk642799gmail.com>
2025-12-02 11:34:23 +0530

    first commit

---
+++ src/main/java/com/ecom/model/Category.java
@@ -0,0 +1,30 @@
+package com.ecom.model;
+import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+
+	private String imageName;
+
+	private Boolean isActive;
+
+}
